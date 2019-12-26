50c2ab0ceac0b7a7bcae9358616b7ff34861f512
Rebecca Tong <rtong@buffalo.edu>
2019-12-26 14:56:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+class Polygon{
+
+  constructor(numbers){
+    console.log(numbers)
+    this.numbers = numbers
+  }
+  get countSides(){
+    //counts number of sides
+    return this.numbers.length
+  }
+
+  get perimeter(){
+    //counts the sum of each side
+  }
+
+}
+
+class Triangle extends Polygon {
+  // inherits constructor from Pet
+}
+
+class Square extends Polygon {
+  // inherits constructor from Pet
+}
