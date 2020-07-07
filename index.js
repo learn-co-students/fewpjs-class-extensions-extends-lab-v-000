8329752c3b8acf489195f03acaeede9cf220f5d0
sjwakeman <swakeman@ca.rr.com>
2020-07-07 12:02:17 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+// has a Polygon class
+class Polygon {
+    constructor(arr) {
+        this.arr = arr;
+    }
+    // counts the number of sides (each index in the array).
+    // Polygon has a countSides getter method 
+    get countSides() {
+        // console.log("this.arr.length", this.arr.length)
+        // this.arr.length 3
+        // that returns the number of sides of the polygon
+        return this.arr.length
+    }   
+    // calculates the sum of each side (each index in the array) of the polygon. 
+    get perimeter() {
+        return this.arr.reduce((sum, arr) => sum + arr, 0);
+    }
+
+}
+// has a Triangle class
+class Triangle extends Polygon {
+
+
+}
