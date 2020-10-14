c67c7b101b41ac092975c576a10bab084bdeb67a
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-13 22:08:47 -0400

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ class Polygon {
     get countSides() {
         return this.array.length;
     }
+
+    get perimeter() {
+        // var per = 0;
+        for ( i = 0; i < this.array.legth; i++ ){
+            return this.array[i];
+        }
+        //  return per;
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        let sideone = (this.array[0]);
+        let sidetwo = (this.array[1]);
+        let sidethree = (this.array[2]);
+        if (sideone + sidetwo > sidethree && sidetwo + sidethree > sideone &&  sidethree + sideone > sidetwo) {
+            return true;
+        } else {
+            return false;
+        }
+    
+    }
+
 }
 
 
