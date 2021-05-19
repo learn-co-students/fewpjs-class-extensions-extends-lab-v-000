84ed1bc02042fac197b436285adb744b7d6ffffb
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-18 22:43:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+class Polygon {
+    constructor(array) {
+        this.sides = array;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce((total, side) => total + side, 0);
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if ((this.countSides === 3) && (this.sides[0] + this.sides[1] > this.sides[2]) && (this.sides[1] + this.sides[2] > this.sides[0]) && this.sides[2] + this.sides[0] > this.sides[1]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        if (this.countSides === 4 && this.sides.every(side => side === this.sides[0])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    get area() {
+        return this.sides[0] ** 2;
+    }
+}
